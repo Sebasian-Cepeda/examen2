bade77d733eb2b53d1baad3631aaf23abcaf8351
juan sebastian cepeda saray <juanydani94@gmail.com>
2024-04-03 20:51:36 -0500

    arreglos proxy

--- src/main/java/com/examen/Main.java
+++ src/main/java/com/examen/Main.java
@@ -1,26 +1,23 @@
 package com.examen;
 
-
 import static spark.Spark.*;
 
 public class Main {
-    public static void main(String... args){
-        port(getPort());
-        staticFileLocation("public");
-        get("/primos", (req,res) -> Service.primos(req.queryParams("value")));
-        get("/factores", (req,res) -> Service.factores(req.queryParams("value")));
+  public static void main(String... args) {
+    port(getPort());
+    // staticFileLocation("public");
+    get("/primos", (req, res) -> Service.primos(req.queryParams("value")));
+    get("/factores", (req, res) -> Service.factores(req.queryParams("value")));
   }
 
-
-  public static String primo(String value){
+  public static String primo(String value) {
     return null;
   }
 
   private static int getPort() {
-      if (System.getenv("PORT") != null) {
-          return Integer.parseInt(System.getenv("PORT"));
-      }
-      return 4567;
+    if (System.getenv("PORT") != null) {
+      return Integer.parseInt(System.getenv("PORT"));
+    }
+    return 4567;
   }
 }
-
