d7db6afd836f3f19e43e8992b6855665de73b9ee
juan sebastian cepeda saray <juanydani94@gmail.com>
2024-04-03 21:45:21 -0500

    add json

--- src/main/java/com/examen/Main.java
+++ src/main/java/com/examen/Main.java
@@ -2,16 +2,28 @@ package com.examen;
 
 import static spark.Spark.*;
 
+import java.util.List;
+
 public class Main {
   public static void main(String... args) {
     port(getPort());
     // staticFileLocation("public");
-    get("/primos", (req, res) -> Service.primos(req.queryParams("value")));
-    get("/factores", (req, res) -> Service.factores(req.queryParams("value")));
-  }
-
-  public static String primo(String value) {
-    return null;
+    get("/primos", (req, res) -> {
+      String value = req.queryParams("value");
+      List<String> output = Service.primos(value);
+      res.type("application/json");
+      if (output.isEmpty()) {
+        return "{\"operation\":\"primes\", \"input\":" + Integer.parseInt(value) + ", \"output\":\"No es primo\"}";
+      } else {
+        return toJson("primes", Integer.parseInt(value), output);
+      }
+    });
+    get("/factores", (req, res) -> {
+      String value = req.queryParams("value");
+      List<String> output = Service.factores(value);
+      res.type("application/json");
+      return toJson("factors", Integer.parseInt(value), output);
+    });
   }
 
   private static int getPort() {
@@ -19,5 +31,15 @@ public class Main {
       return Integer.parseInt(System.getenv("PORT"));
     }
     return 4567;
+  }
+
+  private static String toJson(String operation, int input, List<String> output) {
+    StringBuilder outputStr = new StringBuilder();
+    for (int i = 0; i < output.size() - 1; i++) {
+      outputStr.append(output.get(i)).append(",");
+    }
+    outputStr.append(output.get(output.size() - 1));
+    return "{\"operation\":\"" + operation + "\", \"input\":" + input + ", \"output\":\"" + outputStr.toString()
+        + "\"}";
   }
 }
